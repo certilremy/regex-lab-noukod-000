ed4e8d7244701ebdcd96176bbe887aec4db1a369
Certil Remy <certil@noukod.com>
2018-09-12 19:41:16 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,13 +7,13 @@ text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+text.scan(/^[A-Z].+\W$/) != []
 end
 
 def valid_phone_number?(phone)
-
+phone.scan(/[\d|(]\d+[)|\s|\d]\d+[-|\s|\d]\d+/) != []
 end
