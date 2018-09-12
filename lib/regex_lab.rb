11fed07af81d290222c2718109e585ad40ee528d
Certil Remy <certil@noukod.com>
2018-09-12 19:30:11 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,5 @@
 def starts_with_a_vowel?(word)
-word ="this is some string"
-
- if word.scan(/[aeiou]/)
-   return true
-
- elseif word.scan(/^[^aeiou\W]/)
-   return false
-
- end
+word.scan(/[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
